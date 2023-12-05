1e2d255c33303fb2befa737e73b94deceec38b31
shin <nas221@naver.com>
2023-12-05 14:12:02 +0900

    2023.12.05 snr : 나눗셈 기능 코드 작성

---
+++ Arithmetic/src/com/tj/grp2/Division.java
@@ -0,0 +1,15 @@
+package com.tj.grp2;
+
+public class Division {
+
+	public Division() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int division(int num1, int num2, int num3) {
+		int result = 0;
+		result = (num1 / num2) /num3;
+		
+		return result;
+	}
+}
