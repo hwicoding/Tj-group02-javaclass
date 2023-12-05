d7b58e67df3681e61401e7ae48cfc42322c8a95c
lslh1994 <lh1994@naver.com>
2023-12-05 14:22:05 +0900

    곱셈 작업

---
+++ Arithmetic/src/com/tj/grp2/Multi.java
@@ -0,0 +1,21 @@
+package com.tj.grp2;
+
+public class Multi {
+	
+
+
+		public Multi() {
+		}
+		
+		// Method
+			
+		// 1. 숫자 3개의 곱을 구한다. 
+		public int multiCalc(int firstNum, int secondNum, int thirdNum) {
+			int multi = 0;
+			multi = firstNum * secondNum * thirdNum ;
+			
+			return multi;
+		
+		
+		}
+	}
