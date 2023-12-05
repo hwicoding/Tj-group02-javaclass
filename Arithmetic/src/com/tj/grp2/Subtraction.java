320c1a3fb8351f8c9bec710e84c535758001c8c5
shimhb09 <shimhb09@gmail.com>
2023-12-05 14:10:19 +0900

    뺼셈 작업

---
+++ Arithmetic/src/com/tj/grp2/Subtraction.java
@@ -0,0 +1,17 @@
+package com.tj.grp2;
+
+public class Subtraction {
+
+    public Subtraction() {
+    	
+	}
+
+    public int subCalculator(int num1, int num2, int num3) {
+    	
+    	int minus=0;
+    	minus= num1-num2-num3;
+    	
+    	return minus;
+    }
+
+}
