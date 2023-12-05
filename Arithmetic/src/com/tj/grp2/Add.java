9b140afa6296502f3b2520a7dac82645b599a960
hwicoding <dlgnl117@gmail.com>
2023-12-05 14:11:57 +0900

    덧셈 작업

---
+++ Arithmetic/src/com/tj/grp2/Add.java
@@ -0,0 +1,20 @@
+package com.tj.grp2;
+
+
+public class Add {
+	
+	public Add( ) {
+		
+	}
+	
+	public int addCalculator(int firstNum, int secondNum, int thirdNum ) {
+		
+		int add = 0;
+		add = firstNum + secondNum + thirdNum;
+		
+		
+		return add;
+	}
+	
+
+}
